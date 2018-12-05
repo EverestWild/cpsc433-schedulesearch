f95ab70d1ebebf61e26941e718b6a378dab84baa
Nathan Janzen <moddington@gmail.com>
2018-12-05 12:16:20 -0700

    Implemented OrTreeSearch, minus constraints.

--- src/main/java/schedulesearch/OrTreeSearch.java
+++ src/main/java/schedulesearch/OrTreeSearch.java
@@ -1,22 +1,23 @@
 package schedulesearch;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
-public class OrTreeSearch{
+public class OrTreeSearch {
 
-    public OrTree tree;
-    public OrTree node;
+    public Problem problem;
+    public List<Course> possible_courses = new ArrayList<Course>();
+    public List<Slot> possible_slots = new ArrayList<Slot>();
 
     public OrTreeSearch(ProblemSet problem_set) {
-        // Create a new problem for the root of the tree
-        Problem problem = new Problem(problem_set);
+        problem = new Problem(problem_set);
         // Grab a list of all courses
-        Collection<Course> possible_courses = new ArrayList<Course>();
         possible_courses.addAll(problem_set.courses);
         possible_courses.addAll(problem_set.labs);
+        // Grab a list of all slots
+        possible_slots.addAll(problem_set.course_slots);
+        possible_slots.addAll(problem_set.lab_slots);
         // Insert the partial assignments into the schedule
         for (Pair<Time, Course> partial_assignment : problem_set.partial_assignments) {
             Time time = partial_assignment.getKey();
@@ -31,20 +32,42 @@ public class OrTreeSearch{
             if (slot == null) {
                 throw new RuntimeException("");
             }
-            // Insert the course and remove it from the candidates list
+            // Insert the course and remove it and its slot from the candidate lists
             problem.assignments.add(new Assignment(slot, course));
             possible_courses.remove(course);
+            possible_slots.remove(slot);
         }
-        // Initialize the tree
-        tree = new OrTree(problem, Solution.Unknown, possible_courses);
-        node = tree;
     }
 
-    //Assigns one random course or lab to one random slot and returns whether the search
-    //is complete or not
-    public boolean step(){
-        // TODO
-        return true;
+    public Problem run() {
+        List<Course> courses = new ArrayList<Course>(possible_courses);
+        List<Slot> slots = new ArrayList<Slot>(possible_slots);
+        return searchNode(new Problem(problem), courses, slots);
+    }
+
+    private Problem searchNode(Problem problem, List<Course> possible_courses, List<Slot> possible_slots) {
+        Collections.shuffle(possible_courses);
+        for (Course course : possible_courses) {
+            Collections.shuffle(possible_slots);
+            for (Slot slot : possible_slots) {
+                // Check if this combination would break constraints
+                // TODO
+
+                // Enter this node
+                Problem new_problem = new Problem(problem);
+                List<Course> courses = new ArrayList<Course>(possible_courses);
+                List<Slot> slots = new ArrayList<Slot>(possible_slots);
+                new_problem.assignments.add(new Assignment(slot, course));
+                courses.remove(course);
+                slots.remove(slot);
+
+                Problem result = searchNode(new_problem, courses, slots);
+                if (result != null) {
+                    return result;
+                }
+            }
+        }
+        return null;
     }
 
 }
